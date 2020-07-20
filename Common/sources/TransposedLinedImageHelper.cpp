08541e7ce07d77aa7674d56181a416926f2acb7c
Uzlov Danil <d.uzlov@g.nsu.ru>
2020-07-20 21:47:09 +0700

    Clean code

--- Common/sources/TransposedLinedImageHelper.cpp
+++ Common/sources/TransposedLinedImageHelper.cpp
@@ -13,3 +13,103 @@
 #include "undef.h"
 
 using namespace utils;
+
+void TransposedLinedImageHelper::setBackground(PixelColor value) {
+	backgroundValue = value;
+}
+
+void TransposedLinedImageHelper::setDimensions(index width, index height) {
+	if (this->width == width && this->height == height) {
+		return;
+	}
+	this->width = width;
+	this->height = height;
+
+	beginningOffset = 0;
+
+	const index vectorSize = width * height;
+	maxOffset = getReserveSize(vectorSize);
+
+	pixelData.reserve(vectorSize + maxOffset);
+
+	auto imageLines = getCurrentLinesArray();
+	imageLines.init(backgroundValue);
+
+	lastFillValue = backgroundValue;
+	sameStripsCount = imageLines.getBuffersCount();
+	emptinessWritten = false;
+}
+
+void TransposedLinedImageHelper::pushStrip(array_view<PixelColor> stripData) {
+	sameStripsCount = 0;
+	emptinessWritten = false;
+
+	incrementStrip();
+
+	auto imageLines = getCurrentLinesArray();
+	const index lastStripIndex = width - 1;
+	for (index i = 0; i < stripData.size(); i++) {
+		imageLines[i][lastStripIndex] = stripData[i];
+	}
+}
+
+void TransposedLinedImageHelper::pushEmptyLine(PixelColor value) {
+	if (sameStripsCount >= width) {
+		return;
+	}
+
+	if (sameStripsCount == 0 || lastFillValue != value) {
+		lastFillValue = value;
+		sameStripsCount = 1;
+	} else {
+		sameStripsCount++;
+	}
+	emptinessWritten = false;
+
+	incrementStrip();
+
+	auto imageLines = getCurrentLinesArray();
+	const index lastStripIndex = width - 1;
+	for (index i = 0; i < height; i++) {
+		imageLines[i][lastStripIndex] = value;
+	}
+}
+
+void TransposedLinedImageHelper::write(const string& filepath) const {
+	if (emptinessWritten) {
+		return;
+	}
+
+	BmpWriter::writeFile(filepath, getCurrentLinesArray());
+
+	if (isEmpty()) {
+		emptinessWritten = true;
+	}
+}
+
+index TransposedLinedImageHelper::getReserveSize(index size) {
+	constexpr double reserveCoef = 0.5;
+	return static_cast<index>(std::ceil(size * reserveCoef));
+}
+
+array2d_span<TransposedLinedImageHelper::PixelColor> TransposedLinedImageHelper::getCurrentLinesArray() {
+	return { pixelData.data() + beginningOffset, height, width };
+}
+
+array2d_view<TransposedLinedImageHelper::PixelColor> TransposedLinedImageHelper::getCurrentLinesArray() const {
+	return { pixelData.data() + beginningOffset, height, width };
+}
+
+void TransposedLinedImageHelper::incrementStrip() {
+	if (beginningOffset < maxOffset) {
+		beginningOffset++;
+		return;
+	}
+
+	std::copy(
+		pixelData.data() + beginningOffset + 1,
+		pixelData.data() + beginningOffset + width * height,
+		pixelData.data()
+	);
+	beginningOffset = 0;
+}
--- Common/sources/TransposedLinedImageHelper.h
+++ Common/sources/TransposedLinedImageHelper.h
@@ -8,7 +8,8 @@
  */
 
 #pragma once
-#include "BmpWriter.h"
+#include "array_view.h"
+#include "array2d_view.h"
 
 namespace rxtd::utils {
 	class TransposedLinedImageHelper {
@@ -29,31 +30,9 @@ namespace rxtd::utils {
 
 	public:
 		// Must be called before #setDimensions
-		void setBackground(PixelColor value) {
-			backgroundValue = value;
-		}
+		void setBackground(PixelColor value);
 
-		void setDimensions(index width, index height) {
-			if (this->width == width && this->height == height) {
-				return;
-			}
-			this->width = width;
-			this->height = height;
-
-			beginningOffset = 0;
-
-			const index vectorSize = width * height;
-			maxOffset = getReserveSize(vectorSize);
-
-			pixelData.reserve(vectorSize + maxOffset);
-
-			auto imageLines = getCurrentLinesArray();
-			imageLines.init(backgroundValue);
-
-			lastFillValue = backgroundValue;
-			sameStripsCount = imageLines.getBuffersCount();
-			emptinessWritten = false;
-		}
+		void setDimensions(index width, index height);
 
 		void setWidth(index value) {
 			setDimensions(value, height);
@@ -63,84 +42,24 @@ namespace rxtd::utils {
 			setDimensions(width, value);
 		}
 
-		void pushStrip(array_view<PixelColor> stripData) {
-			sameStripsCount = 0;
-			emptinessWritten = false;
+		void pushStrip(array_view<PixelColor> stripData);
 
-			incrementStrip();
-
-			auto imageLines = getCurrentLinesArray();
-			const index lastStripIndex = width - 1;
-			for (index i = 0; i < stripData.size(); i++) {
-				imageLines[i][lastStripIndex] = stripData[i];
-			}
-		}
-
-		void pushEmptyLine(PixelColor value) {
-			if (sameStripsCount >= width) {
-				return;
-			}
-
-			if (sameStripsCount == 0 || lastFillValue != value) {
-				lastFillValue = value;
-				sameStripsCount = 1;
-			} else {
-				sameStripsCount++;
-			}
-			emptinessWritten = false;
-
-			incrementStrip();
-
-			auto imageLines = getCurrentLinesArray();
-			const index lastStripIndex = width - 1;
-			for (index i = 0; i < height; i++) {
-				imageLines[i][lastStripIndex] = value;
-			}
-		}
+		void pushEmptyLine(PixelColor value);
 
 		bool isEmpty() const {
 			return sameStripsCount >= width;
 		}
 
-		void write(const string& filepath) const {
-			if (emptinessWritten) {
-				return;
-			}
-
-			utils::BmpWriter::writeFile(filepath, getCurrentLinesArray());
-
-			if (isEmpty()) {
-				emptinessWritten = true;
-			}
-		}
+		void write(const string& filepath) const;
 
 	private:
-		static index getReserveSize(index size) {
-			constexpr double reserveCoef = 0.5;
-			return static_cast<index>(std::ceil(size * reserveCoef));
-		}
+		static index getReserveSize(index size);
 
-		utils::array2d_span<PixelColor> getCurrentLinesArray() {
-			return { pixelData.data() + beginningOffset, height, width };
-		}
+		array2d_span<PixelColor> getCurrentLinesArray();
 
-		utils::array2d_view<PixelColor> getCurrentLinesArray() const {
-			return { pixelData.data() + beginningOffset, height, width };
-		}
+		array2d_view<PixelColor> getCurrentLinesArray() const;
 
-		void incrementStrip() {
-			if (beginningOffset < maxOffset) {
-				beginningOffset++;
-				return;
-			}
-
-			std::copy(
-				pixelData.data() + beginningOffset + 1,
-				pixelData.data() + beginningOffset + width * height,
-				pixelData.data()
-			);
-			beginningOffset = 0;
-		}
+		void incrementStrip();
 	};
 
 }
