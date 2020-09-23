cd1dad33b5ef896617b3efbb94b182a6b80e53e4
sctanf <36978460+sctanf@users.noreply.github.com>
2020-09-23 03:28:35 -0500

    experimental support for all multichannel setups

--- AudioAnalyzer/Sources/sound-processing/ChannelMixer.cpp
+++ AudioAnalyzer/Sources/sound-processing/ChannelMixer.cpp
@@ -21,21 +21,37 @@ void ChannelMixer::setLayout(const ChannelLayout& _layout) {
 
 	layout = _layout;
 
-	auto left = layout.indexOf(Channel::eFRONT_LEFT);
-	auto right = layout.indexOf(Channel::eFRONT_RIGHT);
-	auto center = layout.indexOf(Channel::eCENTER);
+	auto afrontLeft = layout.indexOf(Channel::eFRONT_LEFT);
+	auto afrontRight = layout.indexOf(Channel::eFRONT_RIGHT);
+	auto acenter = layout.indexOf(Channel::eCENTER);
+	auto acenterBack = layout.indexOf(Channel::eCENTER_BACK);
+	auto alfe = layout.indexOf(Channel::eLOW_FREQUENCY);
+	auto abackLeft = layout.indexOf(Channel::eBACK_LEFT);
+	auto abackRight = layout.indexOf(Channel::eBACK_RIGHT);
+	auto asideLeft = layout.indexOf(Channel::eSIDE_LEFT);
+	auto asideRight = layout.indexOf(Channel::eSIDE_RIGHT);
 
-	if (left.has_value() && right.has_value()) {
+	if (afrontLeft.has_value() && afrontRight.has_value()) {
 		aliasOfAuto = Channel::eAUTO;
-	} else if (left.has_value()) {
+	} else if (afrontLeft.has_value()) {
 		aliasOfAuto = Channel::eFRONT_LEFT;
-	} else if (right.has_value()) {
+	} else if (afrontRight.has_value()) {
 		aliasOfAuto = Channel::eFRONT_RIGHT;
-	} else if (center.has_value()) {
+	} else if (acenter.has_value()) {
 		aliasOfAuto = Channel::eCENTER;
 	} else {
 		aliasOfAuto = layout.ordered()[0];
 	}
+
+	if (afrontLeft.has_value()) { frontLeft = true; }
+	if (afrontRight.has_value()) { frontRight = true; }
+	if (acenter.has_value()) { center = true; }
+	if (acenterBack.has_value()) { centerBack = true; }
+	if (alfe.has_value()) { lfe = true; }
+	if (abackLeft.has_value()) { backLeft = true; }
+	if (abackRight.has_value()) { backRight = true; }
+	if (asideLeft.has_value()) { sideLeft = true; }
+	if (asideRight.has_value()) { sideRight = true; }
 
 	std::vector<Channel> toDelete;
 	for (const auto& [channel, _] : channels) {
@@ -71,12 +87,27 @@ void ChannelMixer::createAuto() {
 
 	const auto& bufferFirst = channels[Channel::eFRONT_LEFT].getAllData();
 	const auto& bufferSecond = channels[Channel::eFRONT_RIGHT].getAllData();
+	const auto& bufferThird = channels[Channel::eCENTER].getAllData();
+	const auto& bufferFourth = channels[Channel::eCENTER_BACK].getAllData();
+	const auto& bufferFifth = channels[Channel::eLOW_FREQUENCY].getAllData();
+	const auto& bufferSixth = channels[Channel::eBACK_LEFT].getAllData();
+	const auto& bufferSeventh = channels[Channel::eBACK_RIGHT].getAllData();
+	const auto& bufferEighth = channels[Channel::eSIDE_LEFT].getAllData();
+	const auto& bufferNinth = channels[Channel::eSIDE_RIGHT].getAllData();
 
 	const index size = bufferFirst.size();
 	auto writeBuffer = channels[Channel::eAUTO].allocateNext(size);
 
 	for (index i = 0; i < size; ++i) {
-		writeBuffer[i] = (bufferFirst[i] + bufferSecond[i]) * 0.5f;
+		if (frontLeft) { writeBuffer[i] += bufferFirst[i]; }
+		if (frontRight) { writeBuffer[i] += bufferSecond[i]; }
+		if (center) { writeBuffer[i] += bufferThird[i]; }
+		if (centerBack) { writeBuffer[i] += bufferFourth[i]; }
+		if (lfe) { writeBuffer[i] += bufferFifth[i]; }
+		if (backLeft) { writeBuffer[i] += bufferSixth[i]; }
+		if (backRight) { writeBuffer[i] += bufferSeventh[i]; }
+		if (sideLeft) { writeBuffer[i] += bufferEighth[i]; }
+		if (sideRight) { writeBuffer[i] += bufferNinth[i]; }
 	}
 }
 
