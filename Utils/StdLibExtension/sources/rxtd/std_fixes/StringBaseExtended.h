c3935154e18e611c3fcf68a8eb835d56949795c4
Danil <DanilUzlov@yandex.ru>
2021-03-08 10:15:09 +0700

    fix  x86 version not building

--- Utils/StdLibExtension/sources/rxtd/std_fixes/StringBaseExtended.h
+++ Utils/StdLibExtension/sources/rxtd/std_fixes/StringBaseExtended.h
@@ -32,7 +32,9 @@ namespace rxtd::std_fixes {
 
 		constexpr StringViewBaseExtended(const typename base::const_pointer ptr, const size_type count) noexcept : base(ptr, static_cast<typename base::size_type>(count)) { }
 
+#ifndef _WIN32
 		constexpr StringViewBaseExtended(const typename base::const_pointer ptr, const int count) noexcept : StringViewBaseExtended(ptr, static_cast<size_type>(count)) { }
+#endif
 
 		StringViewBaseExtended(const StringBaseExtended<Elem, Traits>&) noexcept;
 
