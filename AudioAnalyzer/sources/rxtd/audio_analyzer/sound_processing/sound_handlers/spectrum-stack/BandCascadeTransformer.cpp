351a2cfa973141dec6eeaacd061df270741f2cc2
Danil <DanilUzlov@yandex.ru>
2021-03-26 19:39:23 +0700

    AudioAnalyzer: refactor BandCascadeTransformer
    Remove option: zeroLevelMultiplier
    Fixdefault value of option minWeight
    Forbid additional handlers between BandsResampler and BandCascadeTransformer

--- AudioAnalyzer/sources/rxtd/audio_analyzer/sound_processing/sound_handlers/spectrum-stack/BandCascadeTransformer.cpp
+++ AudioAnalyzer/sources/rxtd/audio_analyzer/sound_processing/sound_handlers/spectrum-stack/BandCascadeTransformer.cpp
@@ -12,17 +12,17 @@ using ParamsContainer = HandlerBase::ParamsContainer;
 ParamsContainer BandCascadeTransformer::vParseParams(ParamParseContext& context) const noexcept(false) {
 	Params params;
 
-	const float epsilon = std::numeric_limits<float>::epsilon();
-
-	params.minWeight = context.parser.parse(context.options, L"minWeight").valueOr(10.1f);
-	params.minWeight = std::max(params.minWeight, epsilon);
+	params.minWeight = context.parser.parse(context.options, L"minWeight").valueOr(0.0f);
+	if (params.minWeight < 0.0f) {
+		context.log.error(L"minWeight: value must be >= 0 but {} was found", params.minWeight);
+		throw InvalidOptionsException{};
+	}
 
 	params.targetWeight = context.parser.parse(context.options, L"targetWeight").valueOr(2.5f);
-	params.targetWeight = std::max(params.targetWeight, params.minWeight);
-
-	params.zeroLevelHard = context.parser.parse(context.options, L"zeroLevelMultiplier").valueOr(1.0f);
-	params.zeroLevelHard = std::max(params.zeroLevelHard, 0.0f);
-	params.zeroLevelHard *= epsilon;
+	if (params.targetWeight < 0.0f) {
+		context.log.error(L"targetWeight: value must be >= 0 but {} was found", params.targetWeight);
+		throw InvalidOptionsException{};
+	}
 
 	if (const auto mixFunctionString = context.options.get(L"mixFunction").asIString(L"product");
 		mixFunctionString == L"product") {
@@ -30,7 +30,7 @@ ParamsContainer BandCascadeTransformer::vParseParams(ParamParseContext& context)
 	} else if (mixFunctionString == L"average") {
 		params.mixFunction = MixFunction::eAVERAGE;
 	} else {
-		context.log.error(L"mixFunction '{}' is not recognized", mixFunctionString);
+		context.log.error(L"mixFunction: unknown value: '{}', only Product and Average are supported", mixFunctionString);
 		throw InvalidOptionsException{};
 	}
 
@@ -43,7 +43,11 @@ BandCascadeTransformer::vConfigure(const ParamsContainer& _params, Logger& cl, E
 
 	auto& config = getConfiguration();
 
-	resamplerPtr = findBandResampler(config.sourcePtr, cl);
+	resamplerPtr = dynamic_cast<const BandResampler*>(config.sourcePtr);
+	if (resamplerPtr == nullptr) {
+		cl.error(L"BandCascadeTransformer must be attached to BandResampler-type handler");
+		throw InvalidOptionsException{};
+	}
 
 	const auto dataSize = config.sourcePtr->getDataSize();
 
@@ -102,31 +106,13 @@ void BandCascadeTransformer::vProcess(ProcessContext context, ExternalData& exte
 	}
 }
 
-const BandResampler* BandCascadeTransformer::findBandResampler(const HandlerBase* source, Logger& logger) {
-	while (source != nullptr) {
-		auto ptr = dynamic_cast<const BandResampler*>(source);
-		if (ptr != nullptr) {
-			return ptr;
-		}
-		if (dynamic_cast<const BandCascadeTransformer*>(source) != nullptr) {
-			logger.error(L"only one BandCascadeTransformer is allowed in handler chain");
-			throw InvalidOptionsException{};
-		}
-
-		source = source->getConfiguration().sourcePtr;
-	}
-
-	logger.error(L"BandResampler is not found in the source chain");
-	throw InvalidOptionsException{};
-}
-
 float BandCascadeTransformer::computeForBand(index band) const {
 	const BandResampler& resampler = *resamplerPtr;
 
 	float weight = 0.0f;
 	float cascadesSummed = 0.0f;
 
-	float valueProduct = 1.0f;
+	double valueProduct = 1.0;
 	float valueSum = 0.0f;
 
 	const auto bandWeights = resampler.getBandWeights(band);
@@ -135,26 +121,31 @@ float BandCascadeTransformer::computeForBand(index band) const {
 		const float bandWeight = bandWeights[cascade];
 		const float magnitude = snapshot[static_cast<size_t>(cascade)].data[band];
 
-		if (snapshot[static_cast<size_t>(cascade)].maxValue <= params.zeroLevelHard) {
+		if (snapshot[static_cast<size_t>(cascade)].maxValue == 0.0f) {
 			// this most often happens when there was a silence, and then some sound,
 			// so cascade with index more than N haven't updated yet
 			// so in that case we ignore values of all these cascades
 
+			if (cascade == 0) {
+				return 0.0f;
+			}
+
+			// if cascade is not zero and yet cascadesSummed == 0.0f
+			// then bandWeight < params.minWeight for all previous cascades
+			// let's use value of previous cascade to provide at least something
 			if (cascadesSummed == 0.0f) {
-				// if cascadesSummed == 0.0f then bandWeight < params.minWeight for all previous cascades
-				// let's use value of previous cascade to provide at least something
-				return cascade == 0 ? 0.0f : snapshot[static_cast<size_t>(cascade - 1)].data[band];
+				return snapshot[static_cast<size_t>(cascade - 1)].data[band];
 			}
 			break;
 		}
 
-		if (bandWeight < params.minWeight) {
+		if (bandWeight <= params.minWeight) {
 			continue;
 		}
 
 		cascadesSummed += 1.0f;
 
-		valueProduct *= magnitude;
+		valueProduct *= static_cast<double>(magnitude);
 		valueSum += magnitude;
 		weight += bandWeight;
 
@@ -165,16 +156,11 @@ float BandCascadeTransformer::computeForBand(index band) const {
 
 	if (cascadesSummed == 0.0f) {
 		// bandWeight < params.minWeight for all cascades
-		// let's use value of the last cascade with non zero weight and value
-		for (index i = bandWeights.size() - 1; i >= 0; i--) {
-			if (bandWeights[i] > 0.0f && snapshot[static_cast<size_t>(i)].data[band] > static_cast<float>(params.zeroLevelHard)) {
-				return snapshot[static_cast<size_t>(i)].data[band];
-			}
-		}
-		return 0.0f;
+		// let's use value of the last cascade
+		return snapshot.back().data[band];
 	}
 
 	return params.mixFunction == MixFunction::ePRODUCT
-	       ? std::pow(valueProduct, 1.0f / cascadesSummed)
+	       ? static_cast<float>(std::pow(valueProduct, 1.0f / cascadesSummed))
 	       : valueSum / cascadesSummed;
 }
